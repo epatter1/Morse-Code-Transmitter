b989f67e045432ce7ff75716b41e6d9c9d226ab2
epatter1 <elishajp20@gmail.com>
2016-04-05 17:39:22 -0400

    Bare bones- just the layout and nothing else

---
+++ app/src/main/java/edu/ggc/epatter1/morsecodetransmitter/MainActivity.java
@@ -0,0 +1,45 @@
+package edu.ggc.epatter1.morsecodetransmitter;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+//        setSupportActionBar(toolbar);
+
+        findViewById(R.id.transmitButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i("Thank", "You Jesus!");          }
+        });
+
+
+//        ImageButton campusMap = (ImageButton) findViewById(R.id.btnCampusMap);
+//        campusMap.setOnClickListener(new View.OnClickListener() {
+//
+//            @Override
+//            public void onClick(View v) {
+//                startActivity(new Intent(MainActivity.this, CampusMapActivity.class));
+//            }
+//        });
+
+
+
+
+    }
+}
